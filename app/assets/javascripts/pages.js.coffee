3583765954b6bc0aa6dd3fe5130ff44cdb1a8203
Jordan Leigh <AlwaysBCoding@gmail.com>
2012-11-01 16:18:50 -0500

    Player can select a square and it properly unbinds the click event when the AJAX reply comes back

--- app/assets/javascripts/pages.js.coffee
+++ app/assets/javascripts/pages.js.coffee
@@ -1,3 +1,3 @@
 $ ->
   $(".square").on "click", (e) ->
-    $.post '/squares', { x_value: $(e.target).context.dataset["x"], y_value: $(e.target).context.dataset["y"] }
+    $.post '/squares', { x_value: $(e.target).context.dataset["x"], y_value: $(e.target).context.dataset["y"], board: $(e.target).context.dataset["board"] }
--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -1,6 +1,9 @@
 class SquaresController < ApplicationController
   
   def update
-    puts "Received"
+    @square = Square.where(:board_id => params["board"]).where(:x_value => params["x_value"]).where(:y_value => params["y_value"]).first
+    @square.val = "X"; @square.save!
+    
+    render "something.js.erb"
   end
 end
